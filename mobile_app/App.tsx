94c9c75d5db2c694dff23ce5f8b2cc80a55bcbc6
Davorin <davorin.drozg76@gmail.com>
2024-10-30 08:59:55 +0100

    feat: env support added

--- mobile_app/App.tsx
+++ mobile_app/App.tsx
@@ -1,5 +1,6 @@
 import { Platform } from "react-native";
 import RootNavigation from "./navigation";
+import {PARSE_KEY, PARSE_TOKEN, PARSE_SERVER_URL} from "@env";
 
 export default function App({ navigation, route }: any) {
 
---
+++ mobile_app/types/env.d.ts
@@ -0,0 +1,5 @@
+declare module '@env' {
+    export const PARSE_KEY: string
+    export const PARSE_TOKEN: string
+    export const PARSE_SERVER_URL: string
+}
